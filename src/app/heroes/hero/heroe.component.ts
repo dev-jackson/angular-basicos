6032603efd7e8063a0aeadefb52db50ebd1923b4
Angel Sanchez <angeljackson0933@gmail.com>
2022-06-06 19:00:36 -0500

    first commit

---
+++ src/app/heroes/hero/heroe.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './hero.component.html'
+})
+export class HeroeComponent {
+  nombre: string = 'Iroman';
+  edad  : number = 45;
+
+  get nombreCapitalziado(): string{
+    return this.nombre.toUpperCase();
+  }
+  obtenerNombre(): string {
+    return `${this.nombre} - ${this.edad}`;
+  }
+
+  cambiarNombre(): void {
+    this.nombre = 'Spiderman';
+  }
+
+  cambiarEdad(): void {
+    this.edad = 30;
+  }
+}
